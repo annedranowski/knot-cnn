9db407cc3209cebb0ed3a639d512e7f3131a4e48
Refrijirator <77860777+Refrijirator@users.noreply.github.com>
2025-09-30 19:41:25 +0300

    Update eval.py

--- knot_detector/eval.py
+++ knot_detector/eval.py
@@ -1,15 +1,42 @@
 import torch
-from sklearn.metrics import accuracy_score
+from helper_functions import accuracy_fn
 
-def evaluate(model, dataloader, device="cpu"):
+def eval_model(model: torch.nn.Module,
+               data_loader: torch.utils.data.DataLoader,
+               loss_fn: torch.nn.Module,
+               accuracy_fn,
+               device: torch.device = device):
+    """Evaluates a given model on a given dataset.
+
+    Args:
+        model (torch.nn.Module): A PyTorch model capable of making predictions on data_loader.
+        data_loader (torch.utils.data.DataLoader): The target dataset to predict on.
+        loss_fn (torch.nn.Module): The loss function of model.
+        accuracy_fn: An accuracy function to compare the models predictions to the truth labels.
+        device (str, optional): Target device to compute on. Defaults to device.
+
+    Returns:
+        (dict): Results of model making predictions on data_loader.
+    """
+    y_pred_eval = []; y_target_eval = []
+    loss, acc = 0, 0
     model.eval()
-    y_true, y_pred = [], []
-    with torch.no_grad():
-        for xb, yb in dataloader:
-            xb, yb = xb.to(device), yb.to(device)
-            outputs = model(xb)
-            preds = outputs.argmax(dim=1)
-            y_true.extend(yb.cpu().numpy())
-            y_pred.extend(preds.cpu().numpy())
-    acc = accuracy_score(y_true, y_pred)
-    return {"accuracy": acc}
+    with torch.inference_mode():
+        for X, y in data_loader:
+            # Send data to the target device
+            X, y = X.to(device), y.to(device)
+            y_pred = model(X).squeeze(dim=1)
+            loss += loss_fn(y_pred, y)
+            acc += accuracy_fn(y_true=y,
+                                y_pred=y_pred.round())
+
+            X.to('cpu'), y.to('cpu'), y_pred.to('cpu')
+            y_pred_eval.append(y_pred.round())
+            y_target_eval.append(y)
+
+        # Scale loss and acc
+        loss /= len(data_loader)
+        acc /= len(data_loader)
+    return {"model_name": model.__class__.__name__, # only works when model was created with a class
+            "model_loss": loss.item(),
+            "model_acc": acc}, y_pred_eval, y_target_eval
