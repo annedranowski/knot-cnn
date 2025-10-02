7a1c504d42cc9c061de9b06a82c9bb6ba5fc8487
Refrijirator <77860777+Refrijirator@users.noreply.github.com>
2025-10-02 15:04:49 +0300

    Delete knot_detector/training/metrics.py

--- knot_detector/training/metrics.py
+++
@@ -1,4 +0,0 @@
-# knot_detector/training/metrics.py
-import torch
-def binary_round_acc(*, y_true: torch.Tensor, y_pred: torch.Tensor) -> float:
-    return (y_pred.round().eq(y_true).float().mean().item()) * 100.0
